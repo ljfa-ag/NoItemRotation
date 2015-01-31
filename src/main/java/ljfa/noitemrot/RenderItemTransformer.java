9b941b535fe1d2bcf3ad8bc8979a35908ec93aaf
ljfa-ag <ljfa-ag@web.de>
2015-01-31 12:00:42 +0100

    Search for preceding opcode instead
    as there are invisible label and line number
    instruction nodes interspersed

--- src/main/java/ljfa/noitemrot/RenderItemTransformer.java
+++ src/main/java/ljfa/noitemrot/RenderItemTransformer.java
@@ -74,9 +74,9 @@ public class RenderItemTransformer implements IClassTransformer {
             //Search for "fstore 12"
             if(currentNode.getOpcode() == Opcodes.FSTORE) {
                 VarInsnNode node = (VarInsnNode)currentNode;
-                //Check if the argument is 12 and the following instruction is "iconst_1"
-                if(node.var == 12 && currentNode.getNext().getOpcode() == Opcodes.ICONST_1) {
-                    FMLLog.log("NoItemRotation", Level.INFO, "Found target instruction \"fstore 12\" followed by \"iconst_1\"");
+                //Check if the argument is 12 and the preceding instruction is "fmul"
+                if(node.var == 12 && currentNode.getPrevious().getOpcode() == Opcodes.FMUL) {
+                    FMLLog.log("NoItemRotation", Level.INFO, "Found target instruction \"fstore 12\" preceded by \"fmul\"");
 
                     //Remove the 12 preceding instructions
                     for(int i = 0; i < 12; i++)
