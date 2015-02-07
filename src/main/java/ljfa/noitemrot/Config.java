4bace40b2414ef44fd5d314d3c2bc6c92e631f34
ljfa-ag <ljfa-ag@web.de>
2015-02-07 17:24:06 +0100

    Remove the rotateBackwards config
    It's not worth the trouble IMO

--- src/main/java/ljfa/noitemrot/Config.java
+++ src/main/java/ljfa/noitemrot/Config.java
@@ -11,7 +11,6 @@ public class Config {
     
     public static boolean disableRotation;
     public static boolean disableBobbing;
-    public static boolean rotateBackwards;
 
     public static void loadConfig(File file) {
         if(conf == null)
@@ -24,7 +23,6 @@ public class Config {
     public static void loadValues() {
         disableRotation = conf.get(CATEGORY_GENERAL, "disableRotation", true, "Disables items rotating around their axis").getBoolean();
         disableBobbing = conf.get(CATEGORY_GENERAL, "disableBobbing", false, "Disables items bobbing up and down").getBoolean();
-        rotateBackwards = conf.get(CATEGORY_GENERAL, "rotateBackwards", false, "Makes items rotate clockwise rather than counter-clockwise").getBoolean();
         //----------------
         if(conf.hasChanged())
             conf.save();
