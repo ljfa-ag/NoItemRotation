c5a958ae72307cd2f7e429c182f266ec00234744
ljfa-ag <ljfa-ag@web.de>
2015-02-07 17:00:22 +0100

    Create config options

--- src/main/java/ljfa/noitemrot/Config.java
+++ src/main/java/ljfa/noitemrot/Config.java
@@ -9,7 +9,9 @@ public class Config {
     
     public static final String CATEGORY_GENERAL = "general";
     
-    public static boolean 
+    public static boolean disableRotation;
+    public static boolean disableBobbing;
+    public static boolean rotateBackwards;
 
     public static void loadConfig(File file) {
         if(conf == null)
@@ -20,7 +22,9 @@ public class Config {
     }
     
     public static void loadValues() {
-        
+        disableRotation = conf.get(CATEGORY_GENERAL, "disableRotation", true, "Disables items rotating around their axis").getBoolean();
+        disableBobbing = conf.get(CATEGORY_GENERAL, "disableBobbing", false, "Disables items bobbing up and down").getBoolean();
+        rotateBackwards = conf.get(CATEGORY_GENERAL, "rotateBackwards", false, "Makes items rotate clockwise rather than counter-clockwise").getBoolean();
         //----------------
         if(conf.hasChanged())
             conf.save();
